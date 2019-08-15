cb6e73e2be22b3bc74c8e77cdca7d54305dd6936
Dhriaj Sriram <dhirajsriram3194@gmail.com>
2019-08-14 22:14:19 -0400

    changes to loader

--- src/common/Loader.jsx
+++ src/common/Loader.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
-  root:{
-    display:"block",
-    margin:"auto",
-    width:"100%"
+  root: {
+    display: "block",
+    margin: "auto",
+    "@media (max-width: 768px)": {
+      width: "100%"
+    }
   },
   progress: {
-    margin: theme.spacing(2),
-  },
+    margin: theme.spacing(2)
+  }
 }));
 
 export default function CircularIndeterminate() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}> 
-      <img className={classes.root} src={require("../assets/book.gif")} alt="books-loader"/>
+    <div className={classes.root}>
+      <img
+        className={classes.root}
+        src={require("../assets/book.gif")}
+        alt="books-loader"
+      />
     </div>
   );
 }
